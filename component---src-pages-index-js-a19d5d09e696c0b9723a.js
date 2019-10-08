73c37c842a8c0f5bff8d98768fef6e555eea4b4f
Brandon Shewmake <bshewmake@codebrewcafe.com>
2019-10-08 12:59:52 -0500

    Updates

---
+++ component---src-pages-index-js-a19d5d09e696c0b9723a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{216:function(e,n,t){"use strict";t.r(n);var a=t(0),l=t.n(a),c=(t(78),t(214)),o=t(213);n.default=function(){return l.a.createElement(c.a,null,l.a.createElement(o.a,{title:"Home"}),l.a.createElement("h1",null,"Coming Soon"),l.a.createElement("p",null,"We're brewing a special blend at the moment, so check back later."))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-a19d5d09e696c0b9723a.js.map
