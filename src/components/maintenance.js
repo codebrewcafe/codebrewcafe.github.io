4e6b741720c77a9c714a1eaa88a5a4a4c7c93ab2
Brandon Shewmake <bshewmake@codebrewcafe.com>
2019-10-15 20:14:36 -0500

    [minor] Created Maintenance component.

---
+++ src/components/maintenance.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Maintenance = () => (
+  <>
+    <h1>We'll Be Back</h1>
+    <p>We're brewing a special blend at the moment, so check back later.</p>
+  </>
+)
+
+export default Maintenance
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
+import Maintenance from "../components/maintenance"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Coming Soon</h1>
-    <p>We're brewing a special blend at the moment, so check back later.</p>
+    <Maintenance />
   </Layout>
 )
 
